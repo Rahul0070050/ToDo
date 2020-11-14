557852c3c328273a495198dccc685d8dc3cb61e7
Rahul <rahulor2000@gmail.com>
2020-11-13 12:31:07 -1200

    itme Id array completed

--- javaScript/script.js
+++ javaScript/script.js
@@ -8,31 +8,45 @@ var spanDate;
 var contentDiv;
 var contentPTag;
 var destBin;
+var i = 0;
+var listId = [];
 var value;
+
+function arrIds(val) {
+    listId.push(val);
+}
+
 document.getElementById("adding-text-button").addEventListener('click', function () {
     var time = new Date();
     mainContent = document.getElementById("content-writing-space").value;//geting the text from text-aria
-    value = time.getTime();
     container = document.getElementById("todolist-body");
     textUntypeAria = document.getElementById("content-writing-space");
-    listiItemDiv = document.createElement("div");//create 'div' tag
+
+    listiItemDiv = document.createElement("li");//create 'div' tag
     itemHeadDiv = document.createElement("div");//create 'div' tag
     spanItem = document.createElement("span");//create 'span' tag
     spanDate = document.createElement("span");//create 'span' tag
     contentDiv = document.createElement("div");//create 'div' tag
     contentPTag = document.createElement("p");//create 'p' tag
     destBin = document.createElement("i");
+
+
+    destBin.addEventListener("click", function () {
+        container.removeChild(listiItemDiv);
+    })
+
+
     if (mainContent !== "") {
         listiItemDiv.setAttribute("class", "list-item");
-        listiItemDiv.setAttribute("value", value);
-        console.log(value);
+        value = time.getTime();
+        listiItemDiv.setAttribute("id", value);
         itemHeadDiv.setAttribute("class", "item-head");
         spanItem.setAttribute("id", "time");
         spanDate.setAttribute("id", "date");
         contentDiv.setAttribute("class", "content-div");
         destBin.setAttribute("class", "fa fa-trash");
-        destBin.setAttribute("value", value);
-        console.log(value);
+        destBin.setAttribute("id", value);
+        arrIds(value);
 
 
         container.appendChild(listiItemDiv);
@@ -43,7 +57,6 @@ document.getElementById("adding-text-button").addEventListener('click', function
         contentDiv.appendChild(contentPTag);
         contentDiv.appendChild(destBin);
         console.log(listiItemDiv);
-
 
 
         spanDate.innerHTML = time.toDateString();
@@ -60,8 +73,4 @@ document.getElementById("remove-all-Items").addEventListener("click", function (
     document.getElementById("todolist-body").innerHTML = "";
 });
 
-document.getElementById("removeItems").addEventListener("click", function () {
-    if(listiItemDiv.value === destBin.value){
-        console.log(value);
-    }
-});
+
